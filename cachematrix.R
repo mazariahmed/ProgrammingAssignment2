75c289604762af72665347ebb1e70612b07c3a83
mazariahmed <ahmedmazari.ma@gmail.com>
2014-06-22 18:53:50 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,12 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
+makeCacheMatrix <- function(x = numeric()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
 
 ## Write a short comment describing this function
 
